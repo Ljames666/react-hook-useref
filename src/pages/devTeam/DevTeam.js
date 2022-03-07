3d2d916cf3070e722787a6c804fad7d7754e48d6
Jamerson Paz <ljames666@gmail.com>
2022-03-07 15:07:47 -0300

    remove array

--- src/pages/devTeam/DevTeam.js
+++ src/pages/devTeam/DevTeam.js
@@ -1,56 +1,7 @@
 import { Paper, Typography } from '@mui/material/';
 import TeamMain from '../../components/main/Main';
 import ListPerson from '../../components/listPerson/ListPerson';
-
-const developersTeam = [
-  {
-    image: 'https://avatars.githubusercontent.com/u/86246952?v=4',
-    name: 'Jamerson Paz',
-    office: 'Full-Stack Developer | Product Designer',
-    description:
-      'Sou Designer de Produto formado pela UFN, com 8 anos de experiência em criação e desenvolvimento de projetos de moveis e produtos para PDV, e Desenvolvedor Full Stack em formação pela Growdev.',
-    quote: '"Enquanto a Satisfação for maior que a Frustação estou no caminho certo!" Niches,2022.',
-    redeSocial: {
-      twitter: 'https://twitter.com/Ljames666Ljames',
-      facebook: 'https://www.facebook.com/jamerson.paz/',
-      instagram: 'https://www.instagram.com/pazjamerson/',
-      linkedin: 'https://www.linkedin.com/in/jamerson-paz/',
-      github: 'https://github.com/Ljames666/',
-    },
-  },
-  {
-    image: 'https://avatars.githubusercontent.com/u/30914989?v=4',
-    name: 'Lucas Zluhan',
-    office: 'Full-Stack Developer | Mechanic',
-    description:
-      'Olá, sou Lucas, trabalho com mecânica automotiva e estudo desenvolvimento web full-stack na Growdev.',
-    quote:
-      '"Faça como um programador. Quando tudo está errado e confuso, apague tudo e recomece do zero." Orlando Cordeiro.',
-    redeSocial: {
-      twitter: 'https://twitter.com/',
-      facebook: 'https://www.facebook.com/',
-      instagram: 'https://www.instagram.com/lszluhan/',
-      linkedin: 'https://www.linkedin.com/in/lszluhan/',
-      github: 'https://github.com/lucaszluhan',
-    },
-  },
-  {
-    image: 'https://avatars.githubusercontent.com/u/60760830?v=4',
-    name: 'Moisés Benites da silva',
-    office: 'Studant Full-stack developer in the 5th edition of the starter program at Growdev',
-    description:
-      'Sou uma pessoa tranquila, curiosa e que trabalha bem em equipe, ajudando sempre que possível os colegas e buscando conhecimento com quem tem mais experiência.',
-    quote:
-      '"Algo poderoso que aprendi enquanto programador e que devemos aplicar na realidade é: ERRAR RÁPIDO!" Ayrton Yagami.',
-    redeSocial: {
-      twitter: 'https://twitter.com/',
-      facebook: 'https://www.facebook.com/moises.benites.75',
-      instagram: 'https://www.instagram.com/',
-      linkedin: 'https://www.linkedin.com/in/mois%C3%A9s-benites-da-silva-b68358119/',
-      github: 'https://github.com/',
-    },
-  },
-];
+import developersTeam from '../../store/DevelopersTeam';
 
 function DevTeam() {
   return (
